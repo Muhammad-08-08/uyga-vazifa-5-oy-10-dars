a0500152335246fff9faa5fc0c7d623bd49845c1
muhammad <muhammadnuraliyev119@gmail.com>
2025-01-15 15:34:11 +0500

    react

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,32 @@
 import { useState } from "react";
 
+const Timer01Icon = (props) => (
+  <svg
+    xmlns="http://www.w3.org/2000/svg"
+    viewBox="0 0 24 24"
+    width={24}
+    height={24}
+    color={"#ffffff"}
+    fill={"none"}
+    {...props}
+  >
+    <path
+      d="M11.0809 13.152L8 7L13.4196 11.2796C14.1901 11.888 14.1941 13.0472 13.4277 13.6607C12.6614 14.2743 11.5189 14.0266 11.0809 13.152Z"
+      stroke="currentColor"
+      strokeWidth="1.5"
+      strokeLinecap="round"
+      strokeLinejoin="round"
+    />
+    <path
+      d="M5 4.82C3.14864 6.63902 2 9.17385 2 11.9776C2 17.5129 6.47715 22.0001 12 22.0001C17.5228 22.0001 22 17.5129 22 11.9776C22 7.1242 18.5581 3.07656 13.9872 2.15288C13.1512 1.98394 12.7332 1.89947 12.3666 2.20022C12 2.50097 12 2.98714 12 3.95949V4.96175"
+      stroke="currentColor"
+      strokeWidth="1.5"
+      strokeLinecap="round"
+      strokeLinejoin="round"
+    />
+  </svg>
+);
+
 const RefreshIcon = (props) => (
   <svg
     xmlns="http://www.w3.org/2000/svg"
@@ -23,15 +50,15 @@ const RefreshIcon = (props) => (
 function App() {
   const [number, setNumber] = useState(0);
   const [inputValue, setInputValue] = useState(0);
-  const [inc_dec, setInc_dec] = useState(2);
+  const [inc_dec, setInc_dec] = useState(1);
   const [inc_dec_value, setInc_dec_value] = useState(1);
 
   const inc_decClick = () => {
-    setInc_dec(parseInt(inc_dec_value > 100 ? 100 : inc_dec_value));
+    setInc_dec(parseInt(inc_dec_value) > 100 ? 100 : parseInt(inc_dec_value));
   };
-
   const inputValueclick = () => {
-    setNumber(parseInt(inputValue > 100 ? 100 : inputValue));
+    const value = parseInt(inputValue);
+    setNumber(value > 100 ? 100 : value);
     setInputValue("");
   };
   return (
@@ -44,7 +71,7 @@ function App() {
       <div className="flex flex-col gap-1 mt-5">
         <label>Increment/Decrement Value:</label>
         <input
-          onClick={inc_decClick}
+          onKeyUp={inc_decClick}
           value={inc_dec_value}
           onChange={(e) => {
             setInc_dec_value(e.currentTarget.value);
@@ -57,7 +84,7 @@ function App() {
       <div className="flex gap-4 mt-6">
         <button
           onClick={() => {
-            setNumber(number === 0 ? 0 : number - inc_dec);
+            setNumber(number - inc_dec < 0 ? 0 : number - inc_dec);
           }}
           className="h-20 rounded-xl w-20 bg-red-600 text-2xl text-white"
         >
@@ -74,9 +101,7 @@ function App() {
         </button>
         <button
           onClick={() => {
-            console.log(number);
-
-            setNumber(number + inc_dec);
+            setNumber(number + inc_dec > 100 ? 100 : number + inc_dec);
           }}
           className="h-20 rounded-xl w-20 bg-green-600 text-2xl text-white"
         >
@@ -104,7 +129,9 @@ function App() {
       </div>
 
       <button className="h-12 w-full bg-blue-500 text-white text-xl rounded-md mt-4">
-        show history
+        <div className="flex items-center mx-auto w-max h-max gap-3">
+          <Timer01Icon /> show history
+        </div>
       </button>
     </div>
   );
